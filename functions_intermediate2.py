1100d5bdd5c151964cf31eb55dc228e798807dd9
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-06 16:19:59 -0400

    added intermediate functions 2

---
+++ functions_intermediate2.py
@@ -0,0 +1,81 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ]
+x[1][0] = 15
+print(x)
+
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+students[0]['last_name'] = 'Bryant'
+print(students)
+
+# In the sports_directory, change 'Messi' to 'Andres'
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+# Change the value 20 in z to 30
+z[0]['y'] = 30
+print(z)
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+def iterateDictionary(some_list):
+    for i in range(len(some_list)):
+        print(f"first_name - {some_list[i]['first_name']}, last-name - {some_list[i]['last_name']}")
+iterateDictionary(students)
+
+# given a list of dictionaries and a key name, prints the value stored in the key for each dictionary
+def interateDictionary2(keyName, someList):
+    for i in range(len(someList)):
+        print(f"{someList[i][keyName]}")
+interateDictionary2('last_name', students)
+
+# given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(someDict):
+    for key in someDict:
+        print(f"{len(someDict[key])} {key.upper()}")
+        for i in someDict[key]:
+            print(i)
+        print("\n")
+printInfo(dojo)
